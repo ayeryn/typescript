bb382fd8135ce739ce994d59a575aedadbf99828
Eryn Li <erynli@utexas.edu>
2024-09-15 11:40:21 -0500

    nested custom types

--- test.ts
+++ test.ts
@@ -15,16 +15,31 @@ type Person = {
   name: string;
   age: number;
   isStudent: boolean;
+  address: {
+    street: string;
+    city: string;
+    zipCode: number;
+  };
 };
 
 let person: Person = {
   name: "Amy",
   age: 42,
   isStudent: true,
+  address: {
+    street: "123 Main",
+    city: "Ashville",
+    zipCode: 12345,
+  },
 };
 
 let person2: Person = {
   name: "Bob",
   age: 19,
   isStudent: false,
+  address: {
+    street: "456 Market",
+    city: "Boston",
+    zipCode: 65432,
+  },
 };
